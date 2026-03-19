4c2276bb236c9c7fd5f6a5b0218df4b69b75d5ff
raula09 <raul09alazovi@gmail.com>
2026-03-19 21:53:53 +0400

    added comments for better readability(chip8.c)

--- display.h
+++ display.h
@@ -4,15 +4,16 @@
 #include <SDL2/SDL.h>
 #include "chip8.h"
 
+// holds all the SDL stuff we need to keep around
 typedef struct {
     SDL_Window   *window;
     SDL_Renderer *renderer;
-    SDL_Texture  *texture;
+    SDL_Texture  *texture;   // streaming texture we update every frame
 } Display;
 
-void display_init(Display *d, int scale);
-void display_draw(Display *d, Chip8 *chip8);
-void display_handle_input(Chip8 *chip8, SDL_Event *event);
-void display_destroy(Display *d);
+void display_init(Display *d, int scale);           // set up SDL window and renderer
+void display_draw(Display *d, Chip8 *chip8);        // push gfx buffer to the screen
+void display_handle_input(Chip8 *chip8, SDL_Event *event);  // map keys to chip8 keypad
+void display_destroy(Display *d);                   // clean up SDL on exit
 
 #endif
